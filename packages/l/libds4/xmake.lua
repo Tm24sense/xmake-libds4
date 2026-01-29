be75e37c07af0364fdc59e885e8b5c8aa96edbcf
Tm24sense <tayiemallah57@gmail.com>
2026-01-29 14:01:56 +0200

    Wrong hash lol

--- packages/l/libds4/xmake.lua
+++ packages/l/libds4/xmake.lua
@@ -3,11 +3,14 @@ package("libds4")
     set_description("DualShock 4 C/C++ library")
     set_license("MIT")
 
-    -- HEAD only (no tags, no versions)
-    set_urls("https://github.com/Tm24sense/libds4/archive/refs/heads/main.zip")
+    set_urls(
+        "https://github.com/Tm24sense/libds4/archive/refs/tags/v$(version).zip",
+        "https://github.com/Tm24sense/libds4/archive/refs/heads/main.zip"
+    )
+
+    add_versions("1.0.0", "52D0040DB8E854A8A524314805BB144C6703B0EAF596D4178AB789FE362A6662")
     add_deps("hidapi", {configs = {shared = false}})
 
-    -- user options
     add_configs("shared", {
         description = "Build shared library",
         default = false,
