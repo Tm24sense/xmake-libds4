39d24fc8714af2669d76b507a6cd24f045a75cf2
Tm24sense <tayiemallah57@gmail.com>
2026-01-29 13:59:02 +0200

    more tests

--- packages/l/libds4/xmake.lua
+++ packages/l/libds4/xmake.lua
@@ -3,14 +3,11 @@ package("libds4")
     set_description("DualShock 4 C/C++ library")
     set_license("MIT")
 
-    set_urls(
-        "https://github.com/Tm24sense/libds4/archive/refs/tags/v$(version).zip",
-        "https://github.com/Tm24sense/libds4/archive/refs/heads/main.zip"
-    )
-
-    add_versions("1.0.0", "ACF21D152CA1EDFE4DBECF763F57824B45D0CF08EA07EE8F816D391A2177A6AE")
+    -- HEAD only (no tags, no versions)
+    set_urls("https://github.com/Tm24sense/libds4/archive/refs/heads/main.zip")
     add_deps("hidapi", {configs = {shared = false}})
 
+    -- user options
     add_configs("shared", {
         description = "Build shared library",
         default = false,
