7818c1710d9e8f9201255cb61822145ca0bb8bf8
Tm24sense <tayiemallah57@gmail.com>
2026-01-29 14:02:40 +0200

    test

--- packages/l/libds4/xmake.lua
+++ packages/l/libds4/xmake.lua
@@ -8,7 +8,7 @@ package("libds4")
         "https://github.com/Tm24sense/libds4/archive/refs/heads/main.zip"
     )
 
-    add_versions("1.0.0", "52D0040DB8E854A8A524314805BB144C6703B0EAF596D4178AB789FE362A6662")
+    add_versions("1.0.0", "f71c7627")
     add_deps("hidapi", {configs = {shared = false}})
 
     add_configs("shared", {
