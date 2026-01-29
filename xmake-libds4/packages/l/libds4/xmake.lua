14f6ea3b2aee6eb079edbb6ca35873fccefe5e21
Tm24sense <tayiemallah57@gmail.com>
2026-01-29 13:04:38 +0200

    fixes

--- xmake-libds4/packages/l/libds4/xmake.lua
+++ xmake-libds4/packages/l/libds4/xmake.lua
@@ -1,25 +1,28 @@
 package("libds4")
-    set_homepage("https://github.com/YOUR_USERNAME/libds4")
-    set_description("DualShock 4 controller library")
-    set_license("MIT")
-
-    add_urls("https://github.com/YOUR_USERNAME/libds4/archive/refs/tags/$(version).tar.gz",
-             "https://github.com/YOUR_USERNAME/libds4.git")
+    set_homepage("https://github.com/yourusername/libds4")
+    set_description("DS4 library for DualShock 4 controller support")
+    set_license("MIT") 
     
+    add_urls("https://github.com/yourusername/libds4/archive/refs/tags/$(version).tar.gz")
+    add_urls("https://github.com/yourusername/libds4.git")
     
-    add_versions("1.0.0", "52D0040DB8E854A8A524314805BB144C6703B0EAF596D4178AB789FE362A6662")
-
+    add_versions("1.0.0", "ACF21D152CA1EDFE4DBECF763F57824B45D0CF08EA07EE8F816D391A2177A6AE")
+    
     add_deps("hidapi")
-
+    
+    add_configs("shared", {description = "Build shared library", default = false, type = "boolean"})
+    add_configs("build_tests", {description = "Build test executables", default = false, type = "boolean"})
+    
     on_install(function (package)
         local configs = {}
+        
         if package:config("shared") then
-            configs.build_shared = true
+            table.insert(configs, "-DBUILD_SHARED=y")
         end
+        
+        if package:config("build_tests") then
+            table.insert(configs, "-DBUILD_TESTS=y")
+        end
+        
         import("package.tools.xmake").install(package, configs)
     end)
-
-    on_test(function (package)
-        assert(package:has_cfuncs("ds4_init", {includes = "ds4/ds4.h"}))
-    end)
-package_end()
